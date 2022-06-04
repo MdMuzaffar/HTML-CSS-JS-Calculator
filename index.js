f81b2db3afeb30063cff8a29ad301d6cc0dead8a
Muzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2022-06-05 01:14:13 +0530

    JavaScript for calculator
    Responsive calculator with the help of JavaScript

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-let screen = document.getElementById("screen")
-buttons = document.querySelectorAll('button')
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
 
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        console.log('this number cliked', buttonText)
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == "C") {
+            screenValue = '';
+            screen.value = screenValue;
+        } else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        } else {
+            screenValue += buttonText;
+            screen.value = screenValue
+        }
     })
 }
